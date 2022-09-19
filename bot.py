1eda3c71ae81543ce6d5c1347c025c6e5dab1e9f
ImboredTech <113939997+BobDylan24@users.noreply.github.com>
2022-09-19 20:48:16 +0100

    Added creating databases

--- bot.py
+++ bot.py
@@ -1,3 +1,25 @@
+import sqlite3
+import asyncio
 import discord
 from discord.ext import commands
+import configs.secret as secret
 
+intents = discord.Intents.all()
+intents.members = True
+
+bot = commands.Bot(command_prefix=secret.prefix, intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    db = sqlite3.connect("database/db.sqlite")
+    cursor = db.cursor()
+    print("Connected to DB!")
+    cursor.execute("CREATE TABLE IF NOT EXISTS guilds(guildname TEXT, guildid INTEGER)")
+    print("Ran all DB commands.")
+    await asyncio.sleep(2)
+    print(f"Bot is online!\nLogged in as {bot.user.display_name}")
+
+async def main():
+    await bot.start(secret.token)
+
+asyncio.run(main())
